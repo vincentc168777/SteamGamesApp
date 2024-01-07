800649ed3a4cae640d699ecf4a50cdeace29d477
vincentc420 <vinbest777@gmail.com>
2024-01-06 16:08:24 -0800

    basic api request working
    can get info from steam api

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,17 @@
-﻿
-Console.WriteLine("testing");
+﻿using System.Net.Http;
+using System;
+using ConsoleApp1;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        string url = "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=440&count=3&maxlength=300&format=json";
+
+        await WebConnect.GetWebConnectInstance().getSteamInfo(url);
+        
+
+    }
+
+    
+}
---
+++ ConsoleApp1/WebConnect.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    internal sealed class WebConnect
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private static WebConnect connection = new WebConnect();
+        private WebConnect()
+        {
+
+        }
+
+        public static WebConnect GetWebConnectInstance()
+        {
+            return connection;
+        }
+
+        public async Task getSteamInfo(string url)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(data);
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
